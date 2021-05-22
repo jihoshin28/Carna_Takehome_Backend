a809b849170581f3d1b14f39f4b74ff15002fa96
Allen <jihoshin.28@gmail.com>
2021-05-21 18:28:19 -0700

    db folder only for db create

--- dev/pool.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv')
-const Pool = require('pg').Pool
-
-dotenv.config()
-
-const databaseConfig = { connectionString: process.env.DATABASE_URL}
-const pool = new Pool(databaseConfig)
-
-module.exports = pool
